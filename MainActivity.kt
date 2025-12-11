a55456604d59220094075765ef8d3015433b5c12
vivekguptaEngineer <vivekgupta082003@gmail.com>
2025-12-11 16:30:48 +0530

    Refactor MainActivity to use new RecyclerView setup

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,34 +1,42 @@
 package com.example.recycler_view;
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity(){
-    lateinit var Arrylist: ArrayList<News>
-    lateinit var recylc: RecyclerView
+    lateinit var vivek: ArrayList<News>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val NewHeading=arrayOf("pradhu","narayan","gopal","lrishna","vaishnu")
-        val Newsimg=arrayOf(R.drawable.img2
-            ,R.drawable.img1,R.drawable.img3,R.drawable.img4,R.drawable.img5)
-        Arrylist= ArrayList()
-        for (i in Newsimg.indices){
-            val news= News(NewHeading[i],Newsimg[i])
-            Arrylist.add(news)
+
+        val kunal = findViewById<RecyclerView>(R.id.recylc)
+
+        val Text1 = arrayOf("Government curbs IndiGo flights by 10%", "Satya Nadella pledges USD 17.5 billion","Government curbs IndiGo flights by 10%", "Satya Nadella pledges USD 17.5 billion","Government curbs IndiGo flights by 10%","Government curbs IndiGo flights by 10%", "Satya Nadella pledges USD 17.5 billion",)
+        val TextHeading = arrayOf(getString(R.string.Indiago), getString(R.string.Indiago),getString(R.string.Indiago),getString(R.string.Indiago),getString(R.string.Indiago),getString(R.string.Indiago),getString(R.string.Indiago))
+        val inmg = arrayOf(R.drawable.img1, R.drawable.img2,R.drawable.img3,R.drawable.img3,R.drawable.img4,R.drawable.img5,R.drawable.img2,R.drawable.img)
+
+        vivek = ArrayList()
+        for (i in TextHeading.indices){
+            val kuna = News(Text1[i], TextHeading[i], inmg[i])
+            vivek.add(kuna)
         }
-        val list=findViewById<RecyclerView>(R.id.recycle_view)
 
-        list.layoutManager= LinearLayoutManager(null)
+        val myAdapter = MyAdapter(vivek, this)
+        kunal.layoutManager = LinearLayoutManager(this)   // 🔑 Added
+                    // 🔑 Added
+        kunal.adapter = myAdapter
 
-        list.adapter= MyAdapter(Arrylist,this)
-
-intent
-
-
-
-
+        myAdapter.setItemClickListener(object: MyAdapter.onItemClickListener {
+            override fun onItemClick(position: Int) {
+               intent = Intent(this@MainActivity, NewsDetail::class.java)
+                intent.putExtra("heading", vivek[position].NewHeading)
+                intent.putExtra("image", vivek[position].NewImg)
+                intent.putExtra("newsContent", vivek[position].NewsTile)
+                startActivity(intent)
+            }
+        })
     }
 }
