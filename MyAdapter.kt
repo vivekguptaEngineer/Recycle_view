a101bf62cf58cea097738ae9dd9ebec828c03666
vivekguptaEngineer <vivekgupta082003@gmail.com>
2025-12-11 16:31:30 +0530

    Update MyAdapter.kt

--- MyAdapter.kt
+++ MyAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.recycler_view;
 
 import android.app.Activity
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,36 +8,51 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.imageview.ShapeableImageView
 
-class MyAdapter (val kunal : ArrayList<News>,val context: Activity): RecyclerView.Adapter<MyAdapter.MyviewHolder>(){
-    class MyviewHolder(ItemView: View): RecyclerView.ViewHolder(ItemView){
-        val ben=ItemView.findViewById<TextView>(R.id.tvheading_title)
-        val img=ItemView.findViewById<ShapeableImageView>(R.id.heading_img)
-
+class MyAdapter (val vivek: ArrayList<News>,val context: Activity): RecyclerView.Adapter<MyAdapter.MyViewHolder>(){
+private lateinit  var myListener:onItemClickListener
+    interface onItemClickListener{
+        fun onItemClick(position:Int)
+    }
+    fun setItemClickListener(listener: onItemClickListener){
+myListener=listener
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyAdapter.MyviewHolder {
-        val krishna= LayoutInflater.from(context).inflate(R.layout.each_row,parent,false)
-        return MyviewHolder(krishna)
 
 
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MyViewHolder {
+val  sharma= LayoutInflater.from(context).inflate(R.layout.each_row,parent,false)
+        return MyViewHolder(sharma,myListener)
     }
 
-    override fun onBindViewHolder(holder: MyAdapter.MyviewHolder, position: Int) {
-        var current=kunal[position]
-        holder.ben.text=current.newHeading
-        holder.img.setImageResource(kunal[position].newImage)
+    override fun onBindViewHolder(
+        holder: MyViewHolder,
+        position: Int
+    ) {
 
-        holder.itemView.setOnClickListener {
-            val intent = Intent(context, News::class.java)
-            intent.putExtra("heading", current.newHeading)
-            intent.putExtra("image", current.newImage)
-            context.startActivity(intent)
-        }
-
+        holder.vivek_umesh.text=vivek[position].NewsTile
+        holder.gupta.setImageResource(vivek[position].NewImg)
+//        holder.Akash.text=vivek[position].NewHeading
     }
 
     override fun getItemCount(): Int {
-        return kunal.size
+       return vivek.size
     }
+
+    class MyViewHolder(item: View,listener:onItemClickListener): RecyclerView.ViewHolder(item){
+        val gupta=item.findViewById<ShapeableImageView>(R.id.img_type)
+        val vivek_umesh=item.findViewById<TextView>(R.id.TextTile)
+
+        init {
+            item.setOnClickListener {
+            listener.onItemClick(adapterPosition)
+            }
+        }
+
+
+    }
+
 
 }
